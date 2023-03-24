8ce83b033b622681ba101594d5462f8ed7748554
Piki <alexandradesplan@gmail.com>
2023-03-24 15:01:43 +0100

    Activity_main fini

--- app/src/main/java/com/example/projetdessin/MainActivity.java
+++ app/src/main/java/com/example/projetdessin/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.projetdessin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void nouveauDessin(View view)
+    {
+        Intent intent = new Intent(this, DessinActivity.class);
+        startActivity(intent);
+    }
+
+    public void reprendreDessin(View view)
+    {
+        Intent intent = new Intent(this, DessinActivity.class);
+        startActivity(intent);
+    }
+
+    public void quitter(View view)
+    {
+        finish();
+    }
+
 }
